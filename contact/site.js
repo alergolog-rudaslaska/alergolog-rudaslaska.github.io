53cff6b63a083424deff014629ebbfd98af5d9a3
Kus <kus@kus>
2022-06-05 22:06:26 +0200

    init

---
+++ contact/site.js
@@ -0,0 +1,16 @@
+﻿// Write your Javascript code.
+(function () {
+    angular
+		.module("TextAngular", ['textAngular'])
+		.controller("RichTextController", ['$scope', 'textAngularManager', RichTextController]);
+
+    function RichTextController($scope, textAngularManager) {
+
+    };
+})
+();
+
+$('.pull-down').each(function () {
+    var $this = $(this);
+    $this.css('margin-top', $this.parent().height() - $this.height())
+});
